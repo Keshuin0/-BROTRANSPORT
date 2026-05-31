24be2bd904dc0645fcdf753997c6db05b94d1f6d
Keshuin0 <34932346+Keshuin0@users.noreply.github.com>
2026-05-31 16:48:38 -0400

    Initialize Express server to serve static files

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'from';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve compiled static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Route all incoming requests to our main index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server active on port ${PORT}`);
+});
